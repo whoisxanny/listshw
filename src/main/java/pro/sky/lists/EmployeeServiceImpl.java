4dc06a447300fd2c497c5697aff3af36df18c490
whoisxanny <tuffx4nny@gmail.com>
2023-12-24 20:12:21 +0500

    Done?

--- src/main/java/pro/sky/lists/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/lists/EmployeeServiceImpl.java
@@ -1,34 +1,33 @@
 package pro.sky.lists;
 
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.events.Event;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private List<Employee> employeeList;
+    private final Map<String, Employee> employeeList;
 
     public EmployeeServiceImpl() {
-        this.employeeList = new ArrayList<>();
+        employeeList = new HashMap<>();
     }
     @Override
     public Employee add(String surName, String name) {
         Employee employee = new Employee(surName, name);
-        if (employeeList.contains(employee)) {
+        if (employeeList.containsKey(surName + name)) {
             throw new EmployeeAlreadyAddedException("We already have him");
         }
-        employeeList.add(employee);
+        employeeList.put(surName + name, employee);
         return employee;
     }
 
     @Override
     public Employee remove(String surName, String name) {
         Employee employee = new Employee(surName, name);
-        if (employeeList.contains(employee)) {
-            employeeList.remove(employee);
+        if (employeeList.containsKey(surName + name)) {
+            employeeList.remove(surName + name);
             return employee;
         }
         throw new EmployeeNotFoundException("Who is he?");
@@ -37,7 +36,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Employee find(String surName, String name) {
         Employee employee = new Employee(surName, name);
-        if (employeeList.contains(employee)) {
+        if (employeeList.containsKey(surName + name)) {
             return employee;
         }
         throw new EmployeeNotFoundException("Who is he?");
@@ -45,6 +44,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Collection<Employee> findAll() {
-        return new ArrayList<>(employeeList);
+        return Collections.unmodifiableCollection(employeeList.values());
     }
+
 }
