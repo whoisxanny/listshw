5e46f1dcd6701396a6e3f607fff97334e82c72c7
whoisxanny <tuffx4nny@gmail.com>
2023-12-24 16:27:26 +0500

    Initials

---
+++ src/test/java/pro/sky/lists/ListsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.lists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
